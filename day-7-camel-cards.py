4729117da5a57693b6ab4390eb599a6516e46795
codiak <codypumper@gmail.com>
2023-12-07 08:08:31 -0800

    🎁 Day 7: Camel Cards

--- day-7-camel-cards.py
+++ day-7-camel-cards.py
@@ -20,61 +20,71 @@ RE_INT = re.compile(r'[0-9]')
 RE_CARDS = re.compile(r'[2-9AKQJT]')
 card_rank = "23456789TJQKA"
 card_rank_2 = "J23456789TQKA"
+ranks = card_rank
 
-# 7. 5 of a kind
-# 6. 4 of a kind
-# 5. full house
-# 4. 3 of a kind
-# 3. 2 pair
-# 2. 1 pair
-# 1. high card (all unique)
-hands = []
-
-# Evaluate hands
-for i, line in enumerate(lines):
-    parts = line.split(" ")
-    cards = re.findall(RE_CARDS, parts[0])
-    hand = {}
-    for c in cards:
-        if c in hand:
-            hand[c] += 1
+def evaluateHands(lines, j_is_joker=False):
+    hands = []
+    for i, line in enumerate(lines):
+        parts = line.split(" ")
+        cards = re.findall(RE_CARDS, parts[0])
+        hand = {}
+        jokers = 0
+        for c in cards:
+            if c == "J" and j_is_joker:
+                jokers += 1
+            elif c in hand:
+                hand[c] += 1
+            else:
+                hand[c] = 1
+        """
+        Strengths:
+        7. 5 of a kind
+        6. 4 of a kind
+        5. full house (3 of a kind + a pair)
+        4. 3 of a kind
+        3. 2 pair
+        2. 1 pair
+        1. high card (all unique)
+        """
+        strength = 1
+        if jokers == 5:
+            hand["J"] = 5
         else:
-            hand[c] = 1
-    strength = 1
-    # Part 2
-    jokers = hand.get('J', 0)
-    if len(hand) == 1:
-        strength = 7
-    elif len(hand) == 2:
-        if 4 in hand.values():
+            most_of = max(hand, key=hand.get)
+            if jokers > 0:
+                hand[most_of] += jokers
+        if 5 in hand.values():
+            strength = 7
+        elif 4 in hand.values():
             strength = 6
-        else:
-            strength = 5
-    elif len(hand) == 3:
-        if 3 in hand.values():
-            strength = 4
-        else:
-            # 2 pair
-            strength = 3
-    elif len(hand) == 4:
-        strength = 2
-    strength += jokers
-    bid = int(parts[1])
-    hand["strength"] = strength
-    hand["bid"] = bid
-    hand["cards"] = cards
-    hand["jokers"] = jokers
-    hands.append(hand)
+        elif 3 in hand.values():
+            if 2 in hand.values():
+                strength = 5
+            else:
+                strength = 4
+        elif 2 in hand.values():
+            if len(hand) == 3:
+                strength = 3
+            else:
+                strength = 2
+        elif jokers == 1:
+            strength = 2
+        bid = int(parts[1])
+        hand["strength"] = strength
+        hand["bid"] = bid
+        hand["cards"] = cards
+        hands.append(hand)
+    return hands
 
-# Rank hands
+
 def compareHands(a, b):
     if a["strength"] == b["strength"]:
         for i, ac in enumerate(a["cards"]):
             bc = b["cards"][i]
-            if card_rank_2.index(ac) == card_rank_2.index(bc):
+            if ranks.index(ac) == ranks.index(bc):
                 continue
             else:
-                if card_rank_2.index(ac) > card_rank_2.index(bc):
+                if ranks.index(ac) > ranks.index(bc):
                     return 1
                 else:
                     return -1
@@ -84,12 +94,21 @@ def compareHands(a, b):
         else:
             return -1
 
-ranked_hands = sorted(hands, key=functools.cmp_to_key(compareHands))
-
-print(ranked_hands)
-total_winnings = 0
+# Part 1
+hands_1 = evaluateHands(lines, False)
+ranked_hands = sorted(hands_1, key=functools.cmp_to_key(compareHands))
+winnings = 0
 for i, hand in enumerate(ranked_hands):
-    total_winnings += (i+1)*hand["bid"]
+    winnings += (i+1)*hand["bid"]
 
-# 247084283 too low
-print(f"🎁 Part 1, winnings: {total_winnings}")
+print(f"🎁 Part 1, winnings: {winnings}")
+
+# Part 2
+hands_2 = evaluateHands(lines, True)
+ranks = card_rank_2
+ranked_hands = sorted(hands_2, key=functools.cmp_to_key(compareHands))
+winnings = 0
+for i, hand in enumerate(ranked_hands):
+    winnings += (i+1)*hand["bid"]
+
+print(f"🎁 Part 2, winnings: {winnings}")
