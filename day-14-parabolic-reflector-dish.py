1b676408f4950aa9f6a3ce43d370b5de3091f73e
Cody P <codypumper@gmail.com>
2023-12-27 22:56:31 +0100

    🎁 Day 14: Parabolic Reflector

--- day-14-parabolic-reflector-dish.py
+++ day-14-parabolic-reflector-dish.py
@@ -1,5 +1,4 @@
 import numpy as np
-import time
 from numpy.typing import NDArray
 
 from utils import loadInputFile
@@ -24,16 +23,16 @@ lines = text_data.strip().split('\n')
 rock_map: list = []
 rock_arr: NDArray[np.int64] = np.array([])
 chars = { ".": 0, "O": 1,  "#": 2 }
-timing = { 'N': 0, 'S': 0, 'E': 0, 'W': 0}
+arr_at_cycle = []
 
 for y, line in enumerate(lines):
-    # rock_map.append([])
     l = []
     for x, c in enumerate(line):
         l.append(chars[c])
     rock_map.append(np.array(l))
 
 rock_arr = np.array(rock_map)
+
 
 def tilt_up(col):
     prev = None
@@ -52,7 +51,7 @@ def tilt_up(col):
 
 def get_load(arr):
     load = 0
-    cols = arr.T
+    cols = np.rot90(arr, 1)
     for col in cols:
         for i, space in enumerate(col):
             if space == 1:
@@ -65,7 +64,6 @@ def tilt(arr, direction = 'N'):
     if direction == 'N':
         cols = arr.T
     elif direction == 'S':
-        # cols = np.flip(arr.T, 0)
         cols = np.flip(arr, 0).T
     elif direction == 'E':
         cols = np.flip(arr, 1)
@@ -76,37 +74,51 @@ def tilt(arr, direction = 'N'):
     # flip array back
     if direction == 'N':
         arr = cols.T
-        # cols[::-1,::-1].T
     elif direction == 'S':
         arr = np.flip(cols.T, 0)
     elif direction == 'E':
         arr = np.flip(cols, 1)
-        # arr = cols
     else:
         arr = cols
-    timing[direction] += time.time() - start
     return arr
 
+
 def cycle(arr, times = 1):
-    for i in range(times):
-        arr = tilt(arr, 'N')
-        arr = tilt(arr, 'W')
-        arr = tilt(arr, 'S')
-        arr = tilt(arr, 'E')
-        if (rock_arr == arr).all():
-            print(f"Cycle {i+1}, pattern repeats")
+    s = 0
+    dirs = ['N', 'W', 'S', 'E']
+    cycles_skipped = False
+    tilts = 4*times
+    while s < tilts:
+        dir = dirs[s % 4]
+        arr = tilt(arr, dir)
+        if not cycles_skipped and dir == 'E':
+            for j, arr_hash in enumerate(arr_at_cycle):
+                if arr_hash == hash(bytes(arr)):
+                    step = len(arr_at_cycle)
+                    print(f"Step {step} (to {j}), {dir}, pattern repeats")
+                    cyc_len = step - j
+                    remainder = (tilts - s) % cyc_len
+                    s = tilts - remainder - 1
+                    cycles_skipped = True
+                    break
+        arr_at_cycle.append(hash(bytes(arr)))
+        s += 1
     return arr
+
 
 a = tilt(np.copy(rock_arr))
 north_load = get_load(a)
 
 print(f"🎁 Part 1, sum: {north_load}")
 
-
-a2 = cycle(np.copy(rock_arr), 1000)
+a2 = cycle(np.copy(rock_arr), 1000000000)
 load_2 = get_load(a2)
 
-print(f"🎁 Part 2, north support: {np.sum(a2)}")
-print(timing)
-# for row in a2:
-#     print(row)
+# Without cycle detection:
+# .23s for 1000 cycles
+# 293s for 1000000 (example)
+# With:
+# .003s for 1B cycles (example)
+# 7.8s for 1B cycles (input)
+
+print(f"🎁 Part 2, north support: {load_2}")
